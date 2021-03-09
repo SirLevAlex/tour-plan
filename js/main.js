aa08ff62fcbffcfe5a4888ffc45a1c29437473c6
SirLevAlex <boss.lary@mail.ru>
2021-03-09 17:16:54 +0400

    fixed: map

--- js/main.js
+++ js/main.js
@@ -14,16 +14,26 @@ const swiper = new Swiper(".swiper-container", {
 
 ymaps.ready(init);
 function init() {
-  var myMap = new ymaps.Map("map", {
-    center: [7.573638, 79.803893],
-    zoom: 11,
-    controls: ["zoomControl"],
-  }, {
-    searchControlProvider: 'yandex#search'
+  var myMap = new ymaps.Map(
+    "map",
+    {
+      center: [7.573638, 79.803893],
+      zoom: 11,
+      controls: [],
+    },
+  );
+  var zoomControl = new ymaps.control.ZoomControl({
+    options: {
+      position: {
+        right: 10,
+        top: 60
+      },
+    },
   });
-  placemark = new ymaps.Placemark([7.573638, 79.803893], {
-    hintContent: 'Grand Hilton Hotel',
-    balloonContent: '77 Corea Watta, Chilaw 61000, Sri Lanka'
-    }),
-    myMap.geoObjects.add(placemark)
+  myMap.controls.add(zoomControl);
+  (placemark = new ymaps.Placemark([7.573638, 79.803893], {
+    hintContent: "Grand Hilton Hotel",
+    balloonContent: "77 Corea Watta, Chilaw 61000, Sri Lanka",
+  })),
+    myMap.geoObjects.add(placemark);
 }

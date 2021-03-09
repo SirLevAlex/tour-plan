85f0fe85ccccab233e8a97d1abe97b76e66683c4
SirLevAlex <boss.lary@mail.ru>
2021-03-09 16:37:41 +0400

    create: yandex map API

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper(".swiper-container", {
   // Optional parameters
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-    keyboard: {
-      enabled: true,
-      onlyInViewport: false,
-    },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
 });
+
+ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [7.573638, 79.803893],
+    zoom: 11,
+    controls: ["zoomControl"],
+  }, {
+    searchControlProvider: 'yandex#search'
+  });
+  placemark = new ymaps.Placemark([7.573638, 79.803893], {
+    hintContent: 'Grand Hilton Hotel',
+    balloonContent: '77 Corea Watta, Chilaw 61000, Sri Lanka'
+    }),
+    myMap.geoObjects.add(placemark)
+}

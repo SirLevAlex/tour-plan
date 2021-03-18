6d27f8157c9a832f50a1c2ea6d84d53f376e5477
SirLevAlex <boss.lary@mail.ru>
2021-03-18 18:27:10 +0400

    create: send message form by send.php

---
+++ send.php
@@ -0,0 +1,58 @@
+
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+// Формирование самого письма
+$title = "New message Best Tour Plan";
+$body = "
+<h2>New message</h2>
+<b>Name:</b> $name<br>
+<b>Phone:</b> $phone<br><br>
+<b>Message:</b><br>$message
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    // $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'mail.levanboryan.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'besttour@levanboryan.ru'; // Логин на почте
+    $mail->Password   = 'nAc2athix9'; // Пароль на почте
+    $mail->SMTPAutoTLS = false;
+    $mail->SMTPSecure = false;
+    $mail->Port       = 25;
+    $mail->setFrom('besttour@levanboryan.ru', 'Levan Boryan'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('boss.lary@mail.ru');  
+
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: thankyou.html');

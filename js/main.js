f1a628e2792c207e30b918079a1ba3b3a71b6070
SirLevAlex <boss.lary@mail.ru>
2021-03-19 14:29:07 +0400

    create: modal window

--- js/main.js
+++ js/main.js
@@ -1,64 +1,87 @@
-const hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-  effect: "coverflow",
-});
-
-const reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-
-ymaps.ready(init);
-function init() {
-  var myMap = new ymaps.Map("map", {
-    center: [7.573638, 79.803893],
-    zoom: 11,
-    controls: [],
+$(document).ready(function () {
+  const hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+    effect: "coverflow",
   });
-  var zoomControl = new ymaps.control.ZoomControl({
-    options: {
-      position: {
-        right: 10,
-        top: 60,
-      },
+  
+  const reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
     },
   });
-  myMap.controls.add(zoomControl);
-  (placemark = new ymaps.Placemark([7.573638, 79.803893], {
-    hintContent: "Grand Hilton Hotel",
-    balloonContent: "77 Corea Watta, Chilaw 61000, Sri Lanka",
-  })),
-    myMap.geoObjects.add(placemark);
-}
-// паралакс для newsletter
-$(".newsletter").parallax({
-  imageSrc: "./img/newsletter-bg.jpg",
-  zindex: -10,
-  speed: 0.3,
-});
-
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom--visible");
+  
+  ymaps.ready(init);
+  function init() {
+    var myMap = new ymaps.Map("map", {
+      center: [7.573638, 79.803893],
+      zoom: 11,
+      controls: [],
+    });
+    var zoomControl = new ymaps.control.ZoomControl({
+      options: {
+        position: {
+          right: 10,
+          top: 60,
+        },
+      },
+    });
+    myMap.controls.add(zoomControl);
+    (placemark = new ymaps.Placemark([7.573638, 79.803893], {
+      hintContent: "Grand Hilton Hotel",
+      balloonContent: "77 Corea Watta, Chilaw 61000, Sri Lanka",
+    })),
+      myMap.geoObjects.add(placemark);
+  }
+  // паралакс для newsletter
+  $(".newsletter").parallax({
+    imageSrc: "./img/newsletter-bg.jpg",
+    zindex: -10,
+    speed: 0.3,
+  });
+  
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+  });
+  
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $(".modal__close, .modal__overlay");
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+  $(document).keydown(function (press) {
+    if (press.key === 'Escape' || e.keyCode === 27) {
+      closeModal(press);
+    }
+  });
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--active");
+    modalDialog.addClass("modal__dialog--active");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--active");
+    modalDialog.removeClass("modal__dialog--active");
+  }
 });
